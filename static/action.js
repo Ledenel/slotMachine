9c7c5f0f2cdf34f9346e3b211dd3b4136188901a
明阳 <lmysoar@hotmail.com>
2014-12-22 19:54:32 +0800

    开始的时间变得不一样了，8个槽都是可以转动的了

--- static/action.js
+++ static/action.js
@@ -1,27 +1,29 @@
 // this is the main file of the control logic
 
-var draw = function(context) {
-    context.save();
-    context.fillStyle = "rgba(0,0,0,1)";
-    context.fillRect(10, 130, 60, 140);
-    context.fillStyle = "rgba(200,200,200,1)";
-    context.fillRect(15, 135, 50, 130);
-    context.restore();
-};
+//var draw = function(context) {
+//    context.save();
+//    context.fillStyle = "rgba(0,0,0,1)";
+//    context.fillRect(10, 130, 60, 140);
+//    context.fillStyle = "rgba(200,200,200,1)";
+//    context.fillRect(15, 135, 50, 130);
+//    context.restore();
+//};
 
 var num = "12211010";
 
-var drawText = function(context, i) {
-    context.save();
-    context.font = "bold 64px Slackey";
-    context.fillText(num.substr(i,1), 40-20, 200+28);
-    context.restore();
-}
+//var drawText = function(context, i) {
+//    context.save();
+//    context.font = "bold 64px Slackey";
+//    context.fillText(num.substr(i,1), 40-20, 200+28);
+//    context.restore();
+//};
 
-var animate = function(context) {
+var animate = function(context, speed) {
     var num = "0123456789";
     var index = 0;
     var pos = 0;
+    var dist = 10;
+    if(speed)dist = speed;
     setInterval(function() {
         context.save();
         context.clearRect(0, 0, 80, 400);
@@ -30,30 +32,22 @@ var animate = function(context) {
         context.fillText(num[(index+1)%10], 40-20, 160+28+pos);
         context.fillText(num[(index+2)%10], 40-20, 80+28+pos);
         context.restore();
-        pos += 1;
+        pos += dist;
         if (pos >= 80) {
-            pos = 0;
+            pos -= 80;
             index += 1;
+            index %= 10;
         }
     }, 10);
 };
 
 var start = function() {
-//    var slots = $('canvas');
-//    var contexts = $.map(slots, function(slot, n) {
-//        return slot.getContext('2d');
-//    });
-//    contexts.map(draw);
-    var slots = document.getElementsByTagName('canvas');
-    for (var i = 0; i < slots.length; ++i) {
-        var context = slots[i].getContext('2d');
-        //draw(context);
-        //drawText(context, i);
+    $('canvas').each(function (index, ele) {
         setTimeout(function() {
-            animate(context);
-        }, 200+200*i);
-    }
+            animate(ele.getContext('2d'));
+        }, 200+200*index);
+    })
 };
 
 // start after fully loaded
-$(start)
+$(start);
