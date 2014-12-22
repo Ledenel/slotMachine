7e1490cb3d25a48801ded7c48fb4a01cc2370948
songziming <s.ziming@hotmail.com>
2014-12-22 16:24:00 +0800

    Added text rendering

--- static/action.js
+++ static/action.js
@@ -2,10 +2,21 @@
 
 var draw = function(context) {
     context.save();
-    context.fillStyle = "rgba(1,2,3,1)";
+    context.fillStyle = "rgba(0,0,0,1)";
     context.fillRect(10, 130, 60, 140);
+    context.fillStyle = "rgba(200,200,200,1)";
+    context.fillRect(15, 135, 50, 130);
     context.restore();
 };
+
+var num = "12211010";
+
+var drawText = function(context, i) {
+    context.save();
+    context.font = "bold 64px Slackey";
+    context.fillText(num.substr(i,1), 40-20, 200+28);
+    context.restore();
+}
 
 var start = function() {
 //    var slots = $('canvas');
@@ -15,7 +26,9 @@ var start = function() {
 //    contexts.map(draw);
     var slots = document.getElementsByTagName('canvas');
     for (var i = 0; i < slots.length; ++i) {
-        draw(slots[i].getContext('2d'));
+        var context = slots[i].getContext('2d');
+        draw(context);
+        drawText(context, i);
     }
 };
 
