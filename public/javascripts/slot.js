56d83e0c070ddcbad32a181488c16678dcf55ecb
明阳 <lmysoar@hotmail.com>
2014-12-24 00:33:42 +0800

    增加了回弹效果

--- public/javascripts/slot.js
+++ public/javascripts/slot.js
@@ -6,16 +6,21 @@ var IMAGE_TOP_MARGIN = 5;
 var IMAGE_BOTTOM_MARGIN = 5;
 var SLOT_SEPARATOR_HEIGHT = 6
 var SLOT_HEIGHT = IMAGE_HEIGHT + IMAGE_TOP_MARGIN + IMAGE_BOTTOM_MARGIN + SLOT_SEPARATOR_HEIGHT; // how many pixels one slot image takes
-var RUNTIME = 3000; // how long all slots spin before starting countdown
-var SPINTIME = 1000; // how long each slot spins at minimum
-var ITEM_COUNT = 6 // item count in slots
-var SLOT_SPEED = 15; // how many pixels per second slots roll
-var DRAW_OFFSET = 45 // how much draw offset in slot display from top
+//var RUNTIME = 3000; // how long all slots spin before starting countdown
+//var SPINTIME = 1000; // how long each slot spins at minimum
+//var ITEM_COUNT = 6 // item count in slots
+//var SLOT_SPEED = 15; // how many pixels per second slots roll
+//var DRAW_OFFSET = 45 // how much draw offset in slot display from top
 var cnt = -1;
 var stop = [0,0,0,0,0,0,0,0];
 var lucky_star = "12211020";
 var num = "0123456789";
 var drawResult = function (context, index, delta) {
+    context.shadowColor = "rgba(0,0,0,0.5)";
+    context.shadowOffsetX = 5;
+    context.shadowOffsetY = 5;
+    context.shadowBlur = 5;
+    context.font = "bold 64px Slackey";
     var pre = (index+9)%10;
     var now = index;
     var suc = (index+1)%10;
@@ -25,23 +30,34 @@ var drawResult = function (context, index, delta) {
     context.fillText(num[suc], 40-20, 0 * SLOT_HEIGHT + IMAGE_TOP_MARGIN+delta);
     context.restore();
 }
+var vibration = function (context, index) {
+    var count = 0;
+    var limit = 10; //one second
+    var maxDelta = 20; //
+    var dist1 = function(x) {return maxDelta-(maxDelta/limit)*x;};
+    var dist2 = function(x) {return Math.sin(x);};
+    var dist3 = 30;
+    var interval_ID = setInterval(function () {
+        drawResult(context, index, dist1(count)*dist2(count)+dist3);
+        ++count;
+        if(count == limit) {
+            clearInterval(interval_ID);
+            return ;
+        }
+    }, 20);
+}
 var animate = function(context, canvas_ID) {
     var index = 0;
     var pos = 0;
     var startSpeed = -6;
-    var acSpeed = 0.5;
+    var acSpeed = 0.8;
     var maxSpeed = 40;
     var velocity = startSpeed;
     var interval_ID = setInterval(function() {
-        context.save();
-        context.shadowColor = "rgba(0,0,0,0.5)";
-        context.shadowOffsetX = 5;
-        context.shadowOffsetY = 5;
-        context.shadowBlur = 5;
-        context.font = "bold 64px Slackey";
         if(stop[canvas_ID] == 1 && velocity == maxSpeed) {
             drawResult(context, parseInt(lucky_star[canvas_ID]), 30);
             clearInterval(interval_ID);
+            vibration(context, parseInt(lucky_star[canvas_ID]));
             return;
         }
         drawResult(context, index, pos);
@@ -60,12 +76,6 @@ function slotgame() {
     for(var i in stop)stop[i] = 0;
     $("canvas").map(function(index,n){
         var ctx = n.getContext('2d');
-        ctx.save();
-        ctx.shadowColor = "rgba(0,0,0,0.5)";
-        ctx.shadowOffsetX = 5;
-        ctx.shadowOffsetY = 5;
-        ctx.shadowBlur = 5;
-        ctx.font = "bold 64px Slackey";
         drawResult(ctx, 0, 0);
     })
 }
@@ -75,7 +85,7 @@ function rolling(time) {
         $('canvas').each(function (index, ele) {
             setTimeout(function () {
                 animate(ele.getContext('2d'), index);
-            }, 200 + 200 * index);
+            }, 200 + 100 * index);
         })
     }
     else if(cnt < 8 ){
